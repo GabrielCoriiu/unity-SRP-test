065c4ce343ea8657a46f98e86263f1898b763e0a
Gabriel.Coriiu <coriiu.gabriel@gmail.com>
2020-02-29 12:01:35 +0200

    add project resources

---
+++ Assets/Scripts/CameraOpaqueTexturePreview.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.LWRP;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(RawImage))]
+public class CameraOpaqueTexturePreview : MonoBehaviour
+{
+	static readonly int CameraOpaqueTextureID = Shader.PropertyToID("_CameraOpaqueTexture");
+
+	[SerializeField] private Text textLabel = null;
+	private Texture opaqueTexture = null;
+
+	protected void LateUpdate()
+    {
+		if (opaqueTexture != null)
+			return;
+
+		opaqueTexture = Shader.GetGlobalTexture(CameraOpaqueTextureID);
+
+		GetComponent<RawImage>().texture = opaqueTexture;
+		RectTransform rectTransform = (transform as RectTransform);
+		rectTransform.sizeDelta = new Vector2(
+			rectTransform.sizeDelta.y * Screen.width / Screen.height,
+			rectTransform.sizeDelta.y
+		);
+
+		if (textLabel != null && opaqueTexture != null)
+		{
+			textLabel.text = $"" +
+				$"{opaqueTexture.width} x {opaqueTexture.height} \n" +
+				$"supposed to be: {opaqueTexture.width / 4} x {opaqueTexture.height / 4} \n" +
+				$"downsample: {(GraphicsSettings.renderPipelineAsset as LightweightRenderPipelineAsset).opaqueDownsampling}";
+		}
+	}
+}
---
+++ Assets/Scripts/ObjectsSpawner.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class ObjectsSpawner : MonoBehaviour
+{
+	static readonly int BaseColorID = Shader.PropertyToID("_BaseColor");
+
+	[SerializeField] private int amount = 100;
+	[SerializeField] private float spawnRate = 1;
+	[SerializeField] private float spread = 1;
+	[SerializeField] private GameObject prefab = null;
+
+	[Header("GFX")]
+	[SerializeField] private bool randomizeColor = false;
+	[SerializeField] private Gradient randomizeLUT = new Gradient();
+
+	private int count = 0;
+	private float lastSpawnedAtTime = -1f;
+	private MaterialPropertyBlock propertyBlock = null;
+
+	protected void Awake()
+	{
+		propertyBlock = new MaterialPropertyBlock();
+	}
+
+	protected void Update()
+    {
+		float spawnInterval = 1f / spawnRate;
+		if (count < amount && Time.timeSinceLevelLoad - lastSpawnedAtTime >= spawnInterval && SpawnObject())
+		{
+			lastSpawnedAtTime = Time.timeSinceLevelLoad;
+			++count;
+		}
+	}
+
+	protected bool SpawnObject()
+	{
+		if (prefab == null)
+		{
+			Debug.LogError("Please assign a prefab");
+			return false;
+		}
+
+		GameObject obj = Instantiate(
+			prefab,
+			transform.position + Random.insideUnitSphere * spread,
+			Quaternion.identity
+		);
+
+		Renderer renderer = obj.GetComponent<Renderer>();
+		if (renderer != null)
+		{
+			renderer.GetPropertyBlock(propertyBlock);
+			propertyBlock.SetColor(
+				BaseColorID,
+				randomizeColor ? randomizeLUT.Evaluate(Random.value) : randomizeLUT.Evaluate(0f)
+			);
+			renderer.SetPropertyBlock(propertyBlock);
+		}
+
+		return true;
+	}
+}
